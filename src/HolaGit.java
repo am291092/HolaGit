0e43c70c1cb2768c16a4edccf058745e6496ab6e
am291092 <am291092@gmail.com>
2023-04-14 09:56:33 +0200

    esto es una m

--- src/HolaGit.java
+++ src/HolaGit.java
@@ -4,7 +4,7 @@ public class HolaGit {
 	public static void main(String[] args) {
 		System.out.println("Hola Git");
 		System.out.println("Adios Git");
-		System.out.println("");
+		System.out.println("esto es una m");
 
 	}
 
