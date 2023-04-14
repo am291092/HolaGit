fd8bb6f83473ab7ac40f66e00426610714e5c26d
am291092 <am291092@gmail.com>
2023-04-14 09:48:36 +0200

    2

--- src/HolaGit.java
+++ src/HolaGit.java
@@ -4,7 +4,7 @@ public class HolaGit {
 	public static void main(String[] args) {
 		System.out.println("Hola Git");
 		System.out.println("Adios Git");
-		System.out.println("prueba");
+		System.out.println("");
 
 	}
 
